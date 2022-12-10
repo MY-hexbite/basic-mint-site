fc754a0efe2f38f25ea753d221f278c163a32404
Mayada <mayada@hexbite.com>
2022-12-10 11:51:05 -0700

    update mint script to new contract

--- mint.js
+++ mint.js
@@ -13,14 +13,20 @@ let tokensRemaining;
 
 // Address of the selected account
 let account;
+let signer;
 
 let contractNetwork = 5;
-let contractAddress = "0x96a398dd697884f74c321006b7879a604ef89451";
-let mintPrice = 90000000000000000;
-let mintPriceInEther = 0.09;
+let contractAddress = "0x13b1082b95febe2a7257663481e97e9607b35b54";
+let cityId = 1;
+let suburbId = 2;
+let dutchAuctionId;
+let dutchAuctionState;
+// let mintPrice = 90000000000000000;
+// let mintPriceInEther = 0.09;
 
-let maxTokens = 8888;
-let maxPerPurchase = 2;
+let chainIdHex = contractNetwork === 1? "0x1" : "0x5";
+let maxTokens = 10;
+let maxPerPurchase = 1;
 let counterRefreshRate = 120000;
 let chainId;
 let saleState;
@@ -77,7 +83,8 @@ let alertBar = document.getElementById("alert-bar");
 let numAvailableToMint = document.getElementById("available-mint");
 let mintButton = document.querySelector("#mint-button");
 let mintPriceDiv = document.getElementById("mint-price");
-let availableQty = document.getElementById("nfts_minted");
+let availableQty = document.getElementById("nfts-minted");
+let maxSupply = document.getElementById("max-supply");
 let quantityInput = document.getElementById("quantity");
 
 
@@ -130,48 +137,24 @@ let etherscanLink =
     ? "https://etherscan.io/tx"
     : "https://goerli.etherscan.io/tx";
 
-async function updateMintPrice(price) {
+let web3Infura = new Web3(
+  contractNetwork == 1
+    ? "https://mainnet.infura.io/v3/4b48220ef22f43c1a1c842c850869019"
+    : "https://goerli.infura.io/v3/c31e1f10f5e540aeabf40419532cbbb6"
+);
+contract = new web3Infura.eth.Contract(abi, contractAddress);
+
+async function updateMintPrice() {
+  // returns auction price in wei
+  const auctionPrice = await contract.methods
+    .getDutchAuctionPrice(dutchAuctionId)
+    .call();
+
   mintPriceDiv.innerHTML = `Take home a Token for ${
-    ethers.utils.formatEther(price)
+    ethers.utils.formatEther(auctionPrice)
   } eth.`;
-}
 
-async function updateAvailableToMint(account) {
-  let price = mintPrice;
-  mintPriceDiv.innerHTML = '';
-  if (allowListState) {
-    const value = getAmountFromValues(values, account);
-    numAvailableToMint.style.display = "block";
-
-    if (value === undefined) {
-      numAvailableToMint.innerHTML = "You may not mint any tokens until the public sale";
-      availableToMint = 0;
-      maxPerPurchase = availableToMint;
-      return availableToMint;
-    }
-    await updateMintPrice(value.price);
-    const allowListMinted = await contract.methods
-      .getAllowListMinted(account)
-      .call();
-
-    availableToMint = Number(value.amount) - Number(allowListMinted);
-
-    availableToMint = Number(availableToMint);
-    maxPerPurchase = availableToMint;
-
-
-    if (availableToMint === 1) {
-      numAvailableToMint.innerHTML = `You may mint ${availableToMint} token`;
-    } else {
-      numAvailableToMint.innerHTML = `You may mint up to ${availableToMint} tokens`;
-    }
-  // in case of public sale
-  } else {
-    numAvailableToMint.style.display = "block";
-    numAvailableToMint.innerHTML = `You may mint up to ${maxPerPurchase} tokens`;
-    await updateMintPrice(price.toString());
-  }
-  return availableToMint;
+  return auctionPrice;
 }
 
 async function mint() {
@@ -192,9 +175,9 @@ async function mint() {
   erc20 = new ethers.Contract(contractAddress, abi, provider20);
 
   let numberToMint = quantityInput.value;
-  // get price
-  let price = ethers.BigNumber.from(mintPrice.toString());
-  let amountInWei = price.mul(numberToMint);
+
+  // // get price
+  let amountInWei = await updateMintPrice();
 
   document.querySelector("#mint-button").setAttribute("disabled", "disabled");
   mintButton.disabled = true;
@@ -207,11 +190,12 @@ async function mint() {
     value: amountInWei.toString(),
     gasLimit: undefined,
   }
-
-  if (saleState) {
+  // in case of dutch auction mint
+  if (dutchAuctionState) {
     mintButton.innerText = "Minting..";
+
     try {
-      gasEstimate = await erc20.estimateGas.mint(numberToMint, overrides);
+      gasEstimate = await erc20.estimateGas.mintDutch(cityId, suburbId, numberToMint, overrides);
 
       gasEstimate = gasEstimate.mul(
         ethers.BigNumber.from("125").div(ethers.BigNumber.from("100"))
@@ -219,7 +203,7 @@ async function mint() {
 
       overrides.gasLimit = gasEstimate;
 
-      const tx = await contract20.mint(numberToMint, overrides);
+      const tx = await contract20.mintDutch(cityId, suburbId, numberToMint, overrides);
 
       const receipt = await tx.wait();
       const hash = receipt.transactionHash;
@@ -232,123 +216,106 @@ async function mint() {
       createAlert('Canceled transaction.');
       console.log(err);
     };
-  } else if (allowListState && availableToMint !== 0) {
-    const value = getAmountFromValues(values, account);
-    const proof = getProof(tree, value);
 
-    overrides.value = value.price.mul(numberToMint),
-    mintButton.innerText = "Minting..";
+    document.querySelector("#mint-button").removeAttribute("disabled");
+    mintButton.disabled = false;
+    mintButton.innerText = 'Mint';
 
-    try {
-      let gasEstimate = await erc20.estimateGas.mintAllowList(
-        numberToMint,
-        value.amount,
-        value.price,
-        proof,
-        overrides
-      );
-
-      gasEstimate = gasEstimate.mul(ethers.BigNumber.from("125").div(ethers.BigNumber.from("100")))
-      overrides.gasLimit = gasEstimate;
-      const tx = await contract20.mintAllowList(
-        numberToMint,
-        value.amount,
-        value.price,
-        proof,
-        overrides
-      );
-
-      const receipt = await tx.wait();
-      const hash = receipt.transactionHash;
-
-      refreshCounter();
-      createAlert(
-        `Thanks for minting! Your tx link is <a href='${etherscanLink}/${hash}' target="_blank" >${hash.slice(
-          0,
-          6
-        )}...${hash.slice(-4)}</a>`
-      );
-    } catch(err) {
-      createAlert('Canceled transaction.');
-      console.log(err);
-    };
-
-  } else if (allowListState && availableToMint === 0) {
-      mintButton.disabled = true;
-      return;
+    // case of allowlist mint -- to be added
   } else {
-    mintButton.disabled = true;
-    return;
+
   }
-
-  document.querySelector("#mint-button").removeAttribute("disabled");
-  mintButton.disabled = false;
-  mintButton.innerText = 'Mint';
 };
-
-let web3Infura = new Web3(
-  contractNetwork == 1
-    ? "https://mainnet.infura.io/v3/4b48220ef22f43c1a1c842c850869019"
-    : "https://goerli.infura.io/v3/c31e1f10f5e540aeabf40419532cbbb6"
-);
-contract = new web3Infura.eth.Contract(abi, contractAddress);
 
 // checks total minted on the contract
 async function totalSupply() {
   tokensRemaining = await contract.methods.totalSupply().call();
-
   return tokensRemaining;
 }
 
 
 async function refreshCounter() {
-  tokensRemaining = await totalSupply();
+  await totalSupply();
+  await dutchAuctionInfo();
+  await updateMintPrice();
+  await getSuburbs();
+}
+
+async function dutchAuctionInfo() {
+  dutchAuctionId = await contract.methods
+    .getDutchAuctionId(cityId, suburbId)
+    .call();
+
+  let dutchAuctionInfo = await contract.methods
+    .getDutchAuctionInfo(dutchAuctionId)
+    .call();
+
+  dutchAuctionState = dutchAuctionInfo[0];
+}
+
+async function getSuburbs() {
+  // const suburb = await contract.suburbs(1, 1);
+  // // suburb is an object with keys like:
+  // {
+  //   id: {number}, // 1, 2, 3... etc. suburbId local to each city, starts at 1
+  //   cityId: {number}, // parent city
+  //   dutchAuctionId: {number}, // identifier of current DutchAuction, see SteppedDutchAuctionMulti.sol
+  //   englishAuctionId: {number}, // identifier of current EnglishAuction, see EnglishAuctionHouse.sol
+  //   firstTokenId: {number}, // first token in this suburb
+  //   maxSupply: {number}, // max possible supply in the suburb
+  //   currentSupply: {number}, // current supply minted in the suburb
+  //   pricePerToken: {number}, // price on public sale
+  //   allowListPricePerToken: {number}, // price on allow list sale
+  //   allowListActive: {bool}, // is the allow list active? t/f
+  //   saleActive: {bool}, // is the sale active? t/f
+  // }
+
+  const suburbs = await contract.methods
+  .suburbs(cityId, suburbId)
+  .call();
+
+  maxTokens = suburbs.maxSupply;
+  tokensRemaining = suburbs.currentSupply;
+  saleState = suburbs.saleActive;
+  allowListState = suburbs.allowListActive;
+
+  maxSupply.innerText = (maxTokens).toString();
   availableQty.innerText = (maxTokens - tokensRemaining).toString();
-  saleState = await getSaleState();
-  allowListState = await allowList();
 
-  if (account) {
-    availableToMint = await updateAvailableToMint(account);
-  }
-  if (!saleState && !allowListState) {
-    mintButton.disabled = true;
-  } else if (allowListState && availableToMint === 0) {
-    mintButton.disabled = true;
-  } else {
-    mintButton.disabled = false;
-  }
-}
-
-async function getSaleState() {
-  saleState = await contract.methods.saleActive().call();
-  return saleState;
-}
-
-async function allowList() {
-  const allowList = await contract.methods.allowListActive().call();
-  return allowList;
+  return suburbs;
 }
 
 async function fetchAccountData() {
   // Get a Web3 instance for the wallet
-  const web3 = new Web3(provider);
+  let web3 = new Web3(provider);
   contract = await new web3.eth.Contract(abi, contractAddress);
-  console.log('Web3 instance is', web3);
+  clearAlert();
 
   // Get connected chain id from Ethereum node
   chainId = await web3.eth.getChainId();
-
   if (chainId !== contractNetwork) {
     console.log('Incorrect Network');
-    createAlert('Incorrect Network');
-    document.querySelector(".spin").style.display = "none";
-    onDisconnect();
-    return;
+    if (window.ethereum) {
+      try {
+        // check if the chain to connect to is installed
+        await window.ethereum.request({
+          method: "wallet_switchEthereumChain",
+          params: [{ chainId: chainIdHex }], // chainId must be in hexadecimal numbers
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      // if no window.ethereum then MetaMask is not installed
+      console.log("MetaMask is not installed");
+    }
   }
+  clearAlert();
 
   // Get list of accounts of the connected wallet
   const accounts = await web3.eth.getAccounts();
   account = accounts[0];
+  chainId = await web3.eth.getChainId();
 
   document.getElementById("account").style.display = "block";
   document.querySelector("#account").textContent = `${account.slice(
@@ -358,7 +325,7 @@ async function fetchAccountData() {
 
   document.getElementById("mint-section").style.display = "block";
   document.getElementById("btn-connect").style.display = "none";
-  availableToMint = await updateAvailableToMint(account);
+  // availableToMint = await updateAvailableToMint(account);
 
   await refreshCounter();
   mintButton.disabled = false;
@@ -381,21 +348,42 @@ async function refreshAccountData() {
  * Connect wallet button pressed.
  */
 async function onConnect() {
-  console.log("Opening a dialog", web3Modal);
   web3Modal.clearCachedProvider();
   clearAlert();
   try {
     provider = await web3Modal.connect();
-    const web3Provider = new ethers.providers.Web3Provider(provider);
-    const signer = web3Provider.getSigner();
+    let web3 = new Web3(provider);
+    // Get connected chain id from Ethereum node
+    chainId = await web3.eth.getChainId();
+    if (chainId !== contractNetwork) {
+      if (window.ethereum) {
+        try {
+          // check if the chain to connect to is installed
+          await window.ethereum.request({
+            method: "wallet_switchEthereumChain",
+            params: [{ chainId: chainIdHex }], // chainId must be in hexadecimal numbers
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        // if no window.ethereum then MetaMask is not installed
+        console.log("MetaMask is not installed.");
+      }
+    }
+
+    let web3Provider = new ethers.providers.Web3Provider(provider);
+    signer = web3Provider.getSigner();
     account = await signer.getAddress();
+
+    contract = await new web3.eth.Contract(abi, contractAddress);
+    chainId = await web3.eth.getChainId();
 
     document.getElementById("btn-connect").style.display = "none";
     document.getElementById("account").style.display = "block";
     document.getElementById("mint-section").style.display = "block";
 
     await fetchAccountData();
-
   } catch (e) {
     console.log("Could not get a wallet connection", e);
     return;
